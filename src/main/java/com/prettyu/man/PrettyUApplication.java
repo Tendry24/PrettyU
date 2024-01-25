4d29237448784a581f5858df704d7ab6278cf129
Tendry24 <hei.tendry.2@gmail.com>
2024-01-25 18:33:33 +0300

    Initial Commit

---
+++ src/main/java/com/prettyu/man/PrettyUApplication.java
@@ -0,0 +1,13 @@
+package com.prettyu.man;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrettyUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrettyUApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prettyu/man/PrettyUApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prettyu.man;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrettyUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
